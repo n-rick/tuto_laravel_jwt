8bc0160bf940fab2f9984f90922377595cc0ad84
n-rick <e.ficadiere@gmail.com>
2022-07-05 11:35:45 +0200

    chore: frontend > create auth state service

---
+++ frontend/src/app/shared/auth-state.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { TokenService } from './token.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthStateService {
+
+  private userState = new BehaviorSubject<boolean>(this.token.isLoggedIn()!);
+  userAuthState = this.userState.asObservable();
+
+  constructor(private token: TokenService) { }
+  setAuthState(value: boolean) {
+    this.userState.next(value);
+  }
+}
