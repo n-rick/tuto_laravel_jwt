cffed0d9874f911fcbb52f7d8b148a1383578d00
n-rick <e.ficadiere@gmail.com>
2022-07-05 11:57:27 +0200

    chore: frontend > update login form

--- frontend/src/app/components/signin/signin.component.ts
+++ frontend/src/app/components/signin/signin.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthStateService } from 'src/app/shared/auth-state.service';
+import { AuthService } from 'src/app/shared/auth.service';
+import { TokenService } from 'src/app/shared/token.service';
 
 @Component({
   selector: 'app-signin',
@@ -6,10 +11,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
+  loginForm: FormGroup;
+  errors: any = null;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private token: TokenService,
+    private authState: AuthStateService,
+  ) {
+    this.loginForm = this.fb.group({
+      email: [],
+      password: [],
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.authService.signin(this.loginForm.value).subscribe(
+      (res) => {
+        this.responseHandler(res);
+      },
+      (err) => {
+        this.errors = err.error;
+      },
+      () => {
+        this.authState.setAuthState(true);
+        this.loginForm.reset();
+        this.router.navigateByUrl('profile');
+      }
+    );
+  }
+
+  responseHandler(data: any) {
+    // console.log(data);
+    this.token.handleData(data.access_token);
   }
 
 }
