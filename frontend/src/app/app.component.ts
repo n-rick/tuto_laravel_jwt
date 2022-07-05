e314cd47401a92e8703f0c83018a6a2be3348749
n-rick <e.ficadiere@gmail.com>
2022-07-05 12:10:27 +0200

    chore frontend > add logOut

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthStateService } from './shared/auth-state.service';
+import { TokenService } from './shared/token.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+  isSignedIn!: boolean;
+
+  constructor(
+    private auth: AuthStateService,
+    private router: Router,
+    private token: TokenService
+  ) { }
+
+  ngOnInit(): void {
+    this.auth.userAuthState.subscribe((val) => {
+      // console.log(val);
+      this.isSignedIn = val;
+    });
+  }
+
+  // Déconnexion
+  signOut() {
+    this.auth.setAuthState(false);
+    this.token.removeToken();
+    this.router.navigateByUrl('login');
+  }
 }
