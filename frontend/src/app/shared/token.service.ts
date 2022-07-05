da21efeb39830fcee98c8aa3cd1698a2bd0efeb5
n-rick <e.ficadiere@gmail.com>
2022-07-05 11:29:39 +0200

    chore: frontend > create token service

---
+++ frontend/src/app/shared/token.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+  private issuer = {
+    login: 'http://127.0.0.1:8000/api/auth/login',
+    register: 'http://127.0.0.1:8000/api/auth/register',
+  }
+
+  constructor() { }
+
+  handleData(token: any) {
+    localStorage.setItem('auth_token', token);
+  }
+
+  getToken() {
+    return localStorage.getItem('auth_token');
+  }
+
+  isValidToken() {
+    const token = this.getToken();
+    if (token) {
+      const payload = this.payload(token);
+      if (payload) {
+        return Object.values(this.issuer).indexOf(payload.iss) > -1
+          ? true
+          : false;
+      }
+    } else {
+      return false;
+    }
+  }
+  payload(token: any) {
+    const jwtPayload = token.split('.')[1];
+    return JSON.parse(atob(jwtPayload));
+  }
+
+  isLoggedIn() {
+    return this.isValidToken();
+  }
+
+  removeToken() {
+    localStorage.removeItem('auth_token');
+  }
+}
